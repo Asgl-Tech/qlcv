c8dafab11e4e01864f4c6f694590429b419e6b9a
trongtan189 <trongtan189@gmail.com>
2019-12-24 14:36:01 +0700

    tân test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
           <h1 class="text-white text-center">{{ __('Welcome to Material Dashboard FREE Laravel Live Preview.') }}</h1>
-      </div>
+      </div> test
+      
   </div>
 </div>
 @endsection
