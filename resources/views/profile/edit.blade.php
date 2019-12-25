69bcbef4340ed8d7b3fd687b74a46b695ec3ae35
trongtan189 <trongtan189@gmail.com>
2019-12-25 08:39:57 +0700

    Sửa trang profile 1

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -11,7 +11,7 @@
 
             <div class="card ">
               <div class="card-header card-header-primary">
-                <h4 class="card-title">{{ __('Edit Profile') }}</h4>
+                <h4 class="card-title">{{ __('Sửa tài khoản') }}</h4>
                 <p class="card-category">{{ __('User information') }}</p>
               </div>
               <div class="card-body ">
